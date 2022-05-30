f0865c8b7ff22c698b83eb23b7009994284e58f4
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-05-30 15:12:35 +0530

    Delete WatchX.ai directory

--- WatchX.ai/client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import errorReducer from "./errorReducer";
-
-// combines the authReducer and errorReducer
-export default combineReducers({
-  auth: authReducer,
-  errors: errorReducer
-});
