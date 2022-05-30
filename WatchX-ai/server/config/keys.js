8148bdded64a0f727039347d96011abb85f73e45
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-05-31 01:13:39 +0530

    WatchX.ai 🧑‍💻

---
+++ WatchX-ai/server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: "mongodb+srv://vikaspandey:vikaspandey@cluster0.h1lkp.mongodb.net/watchx?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
+
+// mongodb+srv://vikaspandey:<password>@cluster0.h1lkp.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://admin:addiction@cluster0.rcsbb.mongodb.net/online-exam-proctoring?retryWrites=true&w=majorit
