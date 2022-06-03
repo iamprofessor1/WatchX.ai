acdf8ef09ce5a24cef18c33e4419cfaee2ce2f38
Vikas Pandey <86828496+iamprofessor1@users.noreply.github.com>
2022-06-04 01:55:10 +0530

    Delete WatchX-ai directory

--- WatchX-ai/client/src/RegisterLog/types.js
+++
@@ -1,6 +0,0 @@
-// For error .
-export const GET_ERRORS = "GET_ERRORS";
-// For user loading .
-export const USER_LOADING = "USER_LOADING";
-// For setting current user .
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
